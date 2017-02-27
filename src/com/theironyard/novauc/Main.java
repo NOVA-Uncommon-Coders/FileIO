d2f7ebb01e0cbb623608dc4a444292ff282b6cdb
Jeriesha Smith <jerieshasmith@Jerieshas-MBP.fios-router.home>
2017-02-26 19:23:23 -0500

    Updated

--- src/com/theironyard/novauc/Main.java
+++ src/com/theironyard/novauc/Main.java
@@ -1,13 +1,59 @@
 package com.theironyard.novauc;
 
+import jodd.json.JsonParser;
+import jodd.json.JsonSerializer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
+	public static Scanner scanner = new Scanner(System.in);
+	public static Restaurant restaurant = new Restaurant();
 
-    public static void main(String[] args) {
-	String name;
-	String food;
-	String location;
-	String occasion;
-	String setting;
 
-    }
+	public static void main(String[] args) throws Exception, FileNotFoundException {
+
+		try {
+			restaurant = answerQuestions();
+			System.out.println("Save restaurant information.");
+			System.out.println(restaurant);
+			System.out.println("Would you like to update this restaurant information? [y/n]");
+			String answer = scanner.nextLine();
+			if (!answer.equalsIgnoreCase("y")) {
+				return;
+			}
+
+
+		} catch (FileNotFoundException e) {
+			System.out.println("New restaurant input.");
+		}
+		restaurant.answerQuestions();
+		getAnswers();
+	}
+
+	public static void getAnswers() throws IOException {
+		File f = new File("restaurant.json");
+
+		JsonSerializer serializer = new JsonSerializer();
+		String jsonformatted = serializer.serialize(restaurant);
+		FileWriter fw = new FileWriter(f);
+		fw.write(jsonformatted);
+		fw.close();
+
+
+	}
+
+	public static Restaurant answerQuestions() throws FileNotFoundException {
+		Scanner s = new Scanner(System.in);
+		s.useDelimiter("\\Z");
+		String contents = s.next();
+		JsonParser parser = new JsonParser();
+		Restaurant restaurant = parser.parse(contents, Restaurant.class);
+		return restaurant;
+	}
+
+
 }
--- src/com/theironyard/novauc/Restaurant.java
+++ src/com/theironyard/novauc/Restaurant.java
@@ -1,38 +1,93 @@
 package com.theironyard.novauc;
 
+import com.sun.tools.javac.code.Symbol;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+import static com.theironyard.novauc.Main.scanner;
+
 /**
  * Created by jerieshasmith on 2/15/17.
  */
 public class Restaurant {
-    public String name;
-    public String food;
-    public String location;
-    public String occasion;
-    public String setting;
+     String name;
+     String food;
+    String location;
+     String occasion;
+    String setting;
 
-    public static void main(String[] args) throws IOException {
-        File f = new File("restaurant.txt");
-        FileWriter fw = new FileWriter(f);
-        fw.write("Friday's\n");
-        fw.append("Applebees\n");
-        fw.append("Famous Daves\n");
-        fw.close();
+   public void answerQuestions()
 
-        Scanner scanner = new Scanner(f);
-        while (scanner.hasNext()) {
-            System.out.println(scanner.nextLine());
+    {
+        System.out.println("What is the name of the Restaurant?");
+        scanner.nextLine();
+        System.out.println("What type of food?");
+         scanner.nextLine();
+        System.out.println("What is the location?");
+        scanner.nextLine();
+        System.out.println("What is the occasion?");
+        scanner.nextLine();
+        System.out.println("Choice of setting?");
+        scanner.nextLine();
 
 
+    }
+         public Restaurant(){
 
-        }
-        scanner = new Scanner(f);
-        scanner.useDelimiter("\\Z");
-        String contents = scanner.next();
-        System.out.println(contents);
+    }
+
+    public Restaurant(String name, String food, String location, String occasion, String setting){
+             this.name = name;
+             this.food = food;
+             this.location = location;
+             this.occasion = occasion;
+             this.setting = setting;
+    }
+
+    public Restaurant(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getOccasion() {
+        return occasion;
+    }
+
+    public void setOccasion(String occasion) {
+        this.occasion = occasion;
+    }
+
+    public String getSetting() {
+        return setting;
+    }
+
+    public void setSetting(String setting) {
+        this.setting = setting;
     }
 }
